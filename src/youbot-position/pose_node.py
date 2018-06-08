f389b16fe4f703af807bc0af9ec2518f95a60c48
Priya Srikumar <priyasrikumar1@gmail.com>
2018-06-08 12:09:35 -0400

    Fixed naming conventions for topics (mostly)

--- src/youbot-position/pose_node.py
+++ src/youbot-position/pose_node.py
@@ -1,17 +1,18 @@
 import rospy
 from std_msgs.msg import Float64
 
-def callback(vicon_data):
+def callback(vicon):
     global setpoint
-    #get vicon_data
+    pub_x.publish(vicon)
+    #publish pose.x to state
 
 def pose_node():
     rospy.init_node('pose_node')
-    pub_x = rospy.Publisher('pose_node_x', Float64, queue_size=10)
-    #where does publishing to state topic happen?
-    sub = rospy.Subscriber('vicon_data', Float64, callback)
-    while not rospy.is_shutdown():
-        pub_x.publish(vicon_data_x)
+    pub_x = rospy.Publisher('PID_X/state', Float64, queue_size=10)
+    sub = rospy.Subscriber('vicon/youbot/pose', Float64, callback)
+    #while not rospy.is_shutdown():
+    #    pub_x.publish(vicon)
+    #^is above snippet needed? 
 
 if __name__ == '__main__':
         pose_node()
--- src/youbot-position/position_control.py
+++ src/youbot-position/position_control.py
@@ -1,17 +1,37 @@
 #!/usr/bin/env python
 import rospy
+from std_msgs.msg import Float64
+from geometry_msgs.msg import Twist
+
+class Control(data):
+    def __init__(self):
+        self.setpoint = 0
+        self.request_count = 0
+
+    def one_request():
+        if #probably obvious but how to count number of service calls?
+            return true;
+        return false;
 
 def handle_position_control(req):
     print "Point = %"%(req)
-    #return cmd_vel
+    pub_setpoint_x.publish(req.req)
+    #publish req.req to setpoint_x
 
 def callback(data):
-    #get control_x
-    
+    if one_request
+        vel = Twist()
+        vel.linear = data
+        vel.angular = 0 #since for now we're working in one dimension?
+        pub_vel.publish(vel)
+    #get control_x (I think this is x_velocity? Sorry for bad naming)
+    #Also, once we get ^above value, what do we do with it?
+
 def position_control_server():
     rospy.init_node('position_control')
     s = rospy.Service('position_control', PositionControl, handle_position_control)
     pub_setpoint_x = rospy.Publisher('setpoint_x', Float64, queue_size=10)
+    pub_vel = rospy.Publisher('x_velocity', Float64, queue_size = 10)
     sub_control_x = rospy.Subscriber('control_x', Float64, callback)
     rospy.spin()
 
