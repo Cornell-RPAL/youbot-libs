54e1f45073b09ead383502bb9eaa4dd04004446a
Priya Srikumar <priyasrikumar1@gmail.com>
2018-06-11 16:47:52 -0400

    Slight modification to the trial listener

--- src/youbot_position/pose_node.py
+++ src/youbot_position/pose_node.py
@@ -8,14 +8,16 @@ class PoseNode(object):
     def __init__(self):
         self.pub_x = rospy.Publisher('x_pid', Float64, queue_size=10)
         #self.pose_sub = rospy.Subscriber('vicon/youbot/pose', PoseStamped, callback)
-        self.buffer = tf2_ros.Buffer()
-        self.listen_x = tf2_ros.TransformListener(buffer)
 
-    def callback(self, data):
-        transform_x = buffer.lookupTransform( 'x_pid', rospy.Time(0))
-        self.pub_x.publish(transform_x)
 
 if __name__ == '__main__':
   rospy.init_node('pose_node')
   P = PoseNode()
+  #create a listener for the vicon pose information
+  buffer = tf2_ros.Buffer()
+  listen_x = tf2_ros.TransformListener(buffer)
+  #should this be rospy.Time()?
+  transform_x = buffer.lookupTransform(vicon.pose.position.x, 'x_pid', rospy.Time(0))
+  self.pub_x.publish(transform_x)
+
   rospy.spin()
