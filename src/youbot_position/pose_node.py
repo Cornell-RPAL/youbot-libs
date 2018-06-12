b0f2966d123a9bb07c743c0b7bd54b7d7a2550c5
Wil Thomason <wil.thomason@gmail.com>
2018-06-12 16:12:29 -0400

    Make transformlistener and buffer fields

--- src/youbot_position/pose_node.py
+++ src/youbot_position/pose_node.py
@@ -5,20 +5,22 @@ import tf2_ros
 from std_msgs.msg import Float64
 from geometry_msgs.msg import PoseStamped
 
+
 class PoseNode(object):
 
-    def __init__(self):
-        self.pub_x = rospy.Publisher('x_pid', Float64, queue_size=10)
-        #self.pose_sub = rospy.Subscriber('vicon/youbot/pose', PoseStamped, callback)
+  def __init__(self):
+    self.pub_x = rospy.Publisher('x_pid', Float64, queue_size=10)
+    self.buffer = tf2_ros.Buffer()
+    self.listen_x = tf2_ros.TransformListener(self.buffer)
+    #self.pose_sub = rospy.Subscriber('vicon/youbot/pose', PoseStamped, callback)
 
-    def broadcaster(self):
-      buffer = tf2_ros.Buffer()
-      listen_x = tf2_ros.TransformListener(buffer)
-      transform_x = buffer.lookup_transform('/base_footprint', '/base_footprint', rospy.Time(0))
-      self.pub_x.publish(transform_x.transform.translation.x)
+  def broadcaster(self):
+    transform_x = self.buffer.lookup_transform('/base_footprint', '/base_footprint', rospy.Time(0))
+    self.pub_x.publish(transform_x.transform.translation.x)
+
 
 if __name__ == '__main__':
   rospy.init_node('pose_node')
   P = PoseNode()
-  while not rospy.is_shutdown(): 
+  while not rospy.is_shutdown():
     P.broadcaster()
