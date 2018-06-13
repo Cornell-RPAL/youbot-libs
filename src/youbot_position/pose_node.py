83b23c66101b0c38d3ccb54557dc6955b4d9d4fa
Wil Thomason <wil.thomason@gmail.com>
2018-06-13 15:51:36 -0400

    Cleanup and formatting of pose_node

--- src/youbot_position/pose_node.py
+++ src/youbot_position/pose_node.py
@@ -3,22 +3,23 @@
 import rospy
 import tf
 from std_msgs.msg import Float64
-from geometry_msgs.msg import PoseStamped
 
 
 class PoseNode(object):
 
   def __init__(self):
     self.pub_x = rospy.Publisher('x_pid', Float64, queue_size=10)
-    #self.buffer = tf2_ros.Buffer()
+    # self.buffer = tf2_ros.Buffer()
     self.listen_x = tf.TransformListener()
-    #self.pose_sub = rospy.Subscriber('vicon/youbot/pose', PoseStamped, callback)
+    # self.pose_sub = rospy.Subscriber('vicon/youbot/pose', PoseStamped, callback)
 
   def broadcaster(self):
-    (translation, rotation) = self.listen_x.lookupTransform('/base_footprint', '/base_link', rospy.Time(0))
+    (translation, rotation) = self.listen_x.lookupTransform('/base_footprint', '/base_link',
+                                                            rospy.Time(0))
     rospy.loginfo("Successfully looked up transform!")
     self.pub_x.publish(translation[0])
     rospy.loginfo("Successfully published transform info to x_pid!")
+
 
 if __name__ == '__main__':
   rospy.init_node('pose_node')
