699365afe73807b3f0a141bdccec647a65a0acbc
Wil Thomason <wil.thomason@gmail.com>
2018-06-13 16:53:58 -0400

    Fix pose_node by adding a wait for the transform to be available

--- src/youbot_position/pose_node.py
+++ src/youbot_position/pose_node.py
@@ -7,23 +7,32 @@ from std_msgs.msg import Float64
 
 class PoseNode(object):
 
-  def __init__(self):
+  def __init__(self, transform_target='world', transform_source='base_link'):
+    self.transform_target = transform_target
+    self.transform_source = transform_source
     self.pub_x = rospy.Publisher('x_pid', Float64, queue_size=10)
-    # self.buffer = tf2_ros.Buffer()
     self.listen_x = tf.TransformListener()
-    # self.pose_sub = rospy.Subscriber('vicon/youbot/pose', PoseStamped, callback)
 
   def broadcaster(self):
-    (translation, rotation) = self.listen_x.lookupTransform('/base_footprint', '/base_link',
-                                                            rospy.Time(0))
-    rospy.loginfo("Successfully looked up transform!")
-    self.pub_x.publish(translation[0])
-    rospy.loginfo("Successfully published transform info to x_pid!")
+    try:
+      (translation, rotation) = self.listen_x.lookupTransform(self.transform_target,
+                                                              self.transform_source, rospy.Time(0))
+      rospy.loginfo("Successfully looked up transform!")
+      self.pub_x.publish(translation[0])
+      rospy.loginfo("Successfully published transform info to x_pid!")
+    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+      rospy.logerror(
+          "Failed to lookup transform between base_link and map! Got error: {}".format(e))
+
+  def wait(self):
+    rospy.loginfo('Waiting for transform...')
+    self.listen_x.waitForTransform(self.transform_target, self.transform_source, rospy.Time(),
+                                   rospy.Duration(5))
 
 
 if __name__ == '__main__':
   rospy.init_node('pose_node')
   P = PoseNode()
-  rospy.loginfo("About to start the broadcaster loop!")
+  P.wait()
   while not rospy.is_shutdown():
     P.broadcaster()
