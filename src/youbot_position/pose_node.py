a4f6add291bbbb0ef1c7cc68cad3f13f013549d6
Priya Srikumar <priyasrikumar1@gmail.com>
2018-06-13 15:22:45 -0400

    Changed from tf2 to tf

--- src/youbot_position/pose_node.py
+++ src/youbot_position/pose_node.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2.7
 
 import rospy
-import tf2_ros
+import tf
 from std_msgs.msg import Float64
 from geometry_msgs.msg import PoseStamped
 
@@ -10,17 +10,19 @@ class PoseNode(object):
 
   def __init__(self):
     self.pub_x = rospy.Publisher('x_pid', Float64, queue_size=10)
-    self.buffer = tf2_ros.Buffer()
-    self.listen_x = tf2_ros.TransformListener(self.buffer)
+    #self.buffer = tf2_ros.Buffer()
+    self.listen_x = tf.TransformListener()
     #self.pose_sub = rospy.Subscriber('vicon/youbot/pose', PoseStamped, callback)
 
   def broadcaster(self):
-    transform_x = self.buffer.lookup_transform('/base_footprint', '/base_footprint', rospy.Time(0))
-    self.pub_x.publish(transform_x.transform.translation.x)
-
+    (translation, rotation) = self.listen_x.lookupTransform('/base_footprint', '/base_link', rospy.Time(0))
+    rospy.loginfo("Successfully looked up transform!")
+    self.pub_x.publish(translation[0])
+    rospy.loginfo("Successfully published transform info to x_pid!")
 
 if __name__ == '__main__':
   rospy.init_node('pose_node')
   P = PoseNode()
+  rospy.loginfo("About to start the broadcaster loop!")
   while not rospy.is_shutdown():
     P.broadcaster()
