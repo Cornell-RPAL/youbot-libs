4a911751621eb0f38fdd35a4bcc15b592ca1d036
Wil Thomason <wil.thomason@gmail.com>
2018-06-15 13:25:01 -0400

    Tweak log message and formatting in pose_node

--- pose_node.py
+++ pose_node.py
@@ -17,19 +17,19 @@ class PoseNode(object):
   def broadcaster(self):
     try:
       (translation, rotation) = self.listen.lookupTransform(self.transform_target,
-                                                              self.transform_source, rospy.Time(0))
+                                                            self.transform_source, rospy.Time(0))
       rospy.loginfo("Successfully looked up transform!")
       self.pub_x.publish(translation[0])
       self.pub_y.publish(translation[1])
       rospy.loginfo("Successfully published transform info to both PIDS!")
     except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
-      rospy.logerror(
-          "Failed to lookup transform between base_link and map! Got error: {}".format(e))
+      rospy.logerror("Failed to lookup transform between {} and {}! Got error: {}".format(
+          self.transform_source, self.transform_target, e))
 
   def wait(self):
     rospy.loginfo('Waiting for transform...')
     self.listen.waitForTransform(self.transform_target, self.transform_source, rospy.Time(),
-                                   rospy.Duration(5))
+                                 rospy.Duration(5))
 
 
 if __name__ == '__main__':
