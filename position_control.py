77d63c3dfafa17aca338824146662e63e71f9090
Wil Thomason <wil.thomason@gmail.com>
2018-06-15 11:00:35 -0400

    Add stop flag to position control service

--- position_control.py
+++ position_control.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python2.7
 
 import rospy
-from std_msgs.msg import Float64
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
 from youbot_position.srv import PositionControl, PositionControlResponse
 
 
@@ -28,25 +28,24 @@ class Controller(object):
   # Executes when service is called; also sets called to True
   def position_control_service(self, req):
     '''Callback receiving control service requests'''
-    if req.x == 9617.0:
-        null_vector = Twist('[0.0, 0.0, 0.0]', '[0.0, 0.0, 0.0]')
-        self.velocity_pub.publish(null_vector)
-        self.called = False
-        return PositionControlResponse()
-    else:
-        rospy.loginfo("Received request: %s", req)
-        self.setpoint_pub.publish(req.x)
-        self.called = True
-        return PositionControlResponse()
+    if req.stop:
+      null_vector = Twist()
+      self.velocity_pub.publish(null_vector)
+      self.called = False
+      return PositionControlResponse()
+
+    rospy.loginfo("Received request: %s", req)
+    self.setpoint_pub.publish(req.x)
+    self.called = True
+    return PositionControlResponse()
 
   # Check if service has been called. If so, while listening to control_topic,
   # set our linear velocity to the data and publish our current velocity.
   def control_callback(self, control):
     '''Callback receiving PID control output'''
-    if not self.called:
-      return
-    self.velocity.linear.x = control.data
-    self.velocity_pub.publish(self.velocity)
+    if self.called:
+      self.velocity.linear.x = control.data
+      self.velocity_pub.publish(self.velocity)
 
 
 # Spin on control_service so that if the service goes down, we stop. This is better than doing it on
