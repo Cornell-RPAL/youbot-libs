f4126ab735437aedaab378cb9529d1dfb3d6b81a
Wil Thomason <wil.thomason@gmail.com>
2018-06-16 01:13:22 -0400

    Variable renaming and cosmetic changes

--- position_control.py
+++ position_control.py
@@ -10,17 +10,15 @@ class Controller(object):
   '''A position control service node'''
 
   def __init__(self):
-    #, setpoint_topic='setpoint_x', control_topic='control_x'):
+    # setpoint_topic='setpoint_x', control_topic='control_x'):
     # set called to False initially, and iniialize publishers that publish
     # intended setpoint and the desired velocity for the youBot.
-    self.called = False
+    self.set_velocity = False
     self.fresh_x = False
     self.fresh_y = False
     self.setpoint_pub_x = rospy.Publisher('setpoint_x', Float64, queue_size=5, latch=True)
     self.setpoint_pub_y = rospy.Publisher('setpoint_y', Float64, queue_size=5, latch=True)
     self.velocity_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    #  NOTE: I'm storing the velocity as a field because that will make the restructuring easier
-    # when the controller is responsible for both X and Y components of the velocity
     self.velocity = Twist()
     # Initialize subscriber that listens to output of PIDs
     self.control_sub_x = rospy.Subscriber('control_x', Float64, self.control_callback_x)
@@ -30,27 +28,27 @@ class Controller(object):
                                          self.position_control_service)
     rospy.loginfo("Position control service running!")
 
-  # Executes when service is called; also sets called to True
+  # Executes when service is called; sets set_velocity to True
   def position_control_service(self, req):
     '''Callback receiving control service requests'''
     if req.stop:
       null_vector = Twist()
       self.velocity_pub.publish(null_vector)
-      self.called = False
+      self.set_velocity = False
       return PositionControlResponse()
 
     rospy.loginfo("Received request: %s", req)
     self.setpoint_pub_x.publish(req.x)
     self.setpoint_pub_y.publish(req.y)
-    self.called = True
+    self.set_velocity = True
     return PositionControlResponse()
 
   # Check if service has been called. If so, while listening to control_topic,
   # set our linear velocity to the data and publish our current velocity.
   def control_callback_x(self, control):
     '''Callback receiving x PID control output'''
-    if self.called:
       self.velocity.linear.x = control.x
+    if self.set_velocity:
       self.fresh_x = True
       if self.fresh_y:
         self.velocity_pub.publish(self.velocity)
@@ -58,8 +56,8 @@ class Controller(object):
 
   def control_callback_y(self, control):
     '''Callback receiving y PID control output'''
-    if self.called:
       self.velocity.linear.y = control.y
+    if self.set_velocity:
       self.fresh_y = True
       if self.fresh_x:
         self.velocity_pub.publish(self.velocity)
