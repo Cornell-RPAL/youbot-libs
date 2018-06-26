e16c5acd27e348adf15c4bfd18ec71da6e1ee9a7
Priya Srikumar <priyasrikumar1@gmail.com>
2018-06-26 10:19:14 -0400

    Tentative implementation of stopping with Euclidean distance

--- position_control.py
+++ position_control.py
@@ -1,92 +1,98 @@
 #!/usr/bin/env python2.7
 
 import rospy
-import message_filters
+import numpy
+import ast
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Float64
+from std_msgs.msg import Float64, Bool, String
 from youbot_position.srv import PositionControl, PositionControlResponse
-
-STOP_DIST = 0.1
 
 
 class Controller(object):
-  '''A position control service node'''
+    '''A position control service node'''
 
-  def __init__(self):
-    # set called to False initially, and iniialize publishers that publish
-    # intended setpoint and the desired velocity for the youBot.
-    self.set_velocity = False
-    self.fresh_x = False
-    self.fresh_y = False
-    self.setpoint_pub_x = rospy.Publisher('setpoint_x', Float64, queue_size=5, latch=True)
-    self.setpoint_pub_y = rospy.Publisher('setpoint_y', Float64, queue_size=5, latch=True)
-    self.velocity_pub = rospy.Publisher('mufasa/cmd_vel', Twist, queue_size=10)
-    self.velocity = Twist()
-    # Initialize subscriber that listens to output of PIDs
-    self.control_sub_x = message_filters.Subscriber('control_x', Float64)
-    self.control_sub_y = message_filters.Subscriber('control_y', Float64)
-    # Initialize subscriber that listens to position
-    self.pose_sub_x = message_filters.Subscriber('x_pid', Float64)
-    self.pose_sub_y = message_filters.Subscriber('y_pid', Float64)
-    self.ts_x = message_filters.ApproximateTimeSynchronizer(
-        [self.control_sub_x, self.pose_sub_x], 10, 0.1, allow_headerless=True)
-    self.ts_x.registerCallback(self.control_callback_x)
-    self.ts_y = message_filters.ApproximateTimeSynchronizer(
-        [self.control_sub_y, self.pose_sub_y], 10, 0.1, allow_headerless=True)
-    self.ts_y.registerCallback(self.control_callback_y)
-    # Initialize service that will accept requested positions and set PID setpoints to match
-    self.control_service = rospy.Service('position_control', PositionControl,
-                                         self.position_control_service)
-    rospy.loginfo("Position control service running!")
-
-  # Executes when service is called; sets set_velocity to True
-  def position_control_service(self, req):
-    '''Callback receiving control service requests'''
-    if req.stop:
-      null_vector = Twist()
-      self.velocity_pub.publish(null_vector)
-      self.set_velocity = False
-      return PositionControlResponse()
-
-    rospy.loginfo("Received request: %s", req)
-    self.setpoint_pub_x.publish(req.x)
-    self.setpoint_pub_y.publish(req.y)
-    self.set_velocity = True
-    return PositionControlResponse()
-
-  # Check if service has been called. If so, while listening to control_topic,
-  # set our linear velocity to the data and publish our current velocity.
-  def control_callback_x(self, control_x, x_pid, req):
-    '''Callback receiving x PID control output'''
-    if self.set_velocity:
-      self.velocity.linear.x = -control_x.data
-      self.fresh_x = True
-      if self.fresh_y:
-        self.velocity_pub.publish(self.velocity)
+    def __init__(self):
+        # setpoint_topic='setpoint_x', control_topic='control_x'):
+        # set called to False initially, and iniialize publishers that publish
+        # intended setpoint and the desired velocity for the youBot.
+        self.set_velocity = False
         self.fresh_x = False
-      if abs(req.x - x_pid.data) <= STOP_DIST:
-        self.velocity.linear.x = 0.0
-        self.velocity_pub.publish(self.velocity)
-        self.fresh_x = True
-
-  def control_callback_y(self, control_y, y_pid, req):
-    '''Callback receiving y PID control output'''
-    if self.set_velocity:
-      self.velocity.linear.y = -control_y.data
-      self.fresh_y = True
-      if self.fresh_x:
-        self.velocity_pub.publish(self.velocity)
         self.fresh_y = False
-      if abs(req.y - y_pid.data) <= STOP_DIST:
-        self.velocity.linear.y = 0.0
-        self.velocity_pub.publish(self.velocity)
-        self.fresh_y = True
+        self.setpoint_pub_x = rospy.Publisher(
+            'setpoint_x', Float64, queue_size=5, latch=True)
+        self.setpoint_pub_y = rospy.Publisher(
+            'setpoint_y', Float64, queue_size=5, latch=True)
+        self.velocity_pub = rospy.Publisher('mufasa/cmd_vel', Twist, queue_size=10)
+        # Initialize publishers to pid_enable topics
+        self.pid_x_pub = rospy.Publisher('enable_x', Bool, queue_size=10)
+        self.pid_y_pub = rospy.Publisher('enable_y', Bool, queue_size=10)
+        self.velocity = Twist()
+        # Initialize subscriber that listens to output of PIDs
+        self.control_sub_x = rospy.Subscriber(
+            'control_x', Float64, self.control_callback_x)
+        self.control_sub_y = rospy.Subscriber(
+            'control_y', Float64, self.control_callback_y)
+        # Initialize subsriber to pose topic
+        self.pose_sub = rospy.Subscriber('pos', String, self.pose_callback)
+        # Initialize service that will accept requested positions and set PID setpoints to match
+        self.control_service = rospy.Service('position_control', PositionControl,
+                                             self.position_control_service)
+        rospy.loginfo("Position control service running!")
+
+    # Executes when service is called; sets set_velocity to True
+    def position_control_service(self, req):
+        '''Callback receiving control service requests'''
+        if req.stop:
+            null_vector = Twist()
+            self.velocity_pub.publish(null_vector)
+            self.set_velocity = False
+            return PositionControlResponse()
+
+        rospy.loginfo("Received request: %s", req)
+        self.setpoint_pub_x.publish(req.x)
+        self.setpoint_pub_y.publish(req.y)
+        self.pid_x_pub.publish(True)
+        self.pid_y_pub.publish(True)
+        self.set_velocity = True
+        return PositionControlResponse()
+
+    # Check if service has been called. If so, while listening to control_topic,
+    # set our linear velocity to the data and publish our current velocity.
+    def control_callback_x(self, control_x):
+        '''Callback receiving x PID control output'''
+        if self.set_velocity:
+            self.velocity.linear.x = control_x.data
+            self.fresh_x = True
+            if self.fresh_y:
+                self.velocity_pub.publish(self.velocity)
+                self.fresh_x = False
+
+    def control_callback_y(self, control_y):
+        '''Callback receiving y PID control output'''
+        if self.set_velocity:
+            self.velocity.linear.y = control_y.data
+            self.fresh_y = True
+            if self.fresh_x:
+                self.velocity_pub.publish(self.velocity)
+                self.fresh_y = False
+
+
+    def pose_callback(self, pos):
+        pose = ast.literal_eval(str(pos)[7:-21])
+        dist = numpy.linalg.norm(pose)
+        rospy.loginfo("Got distance: %s", dist)
+        if (dist <= 0.05):
+            self.set_velocity = False
+            self.pid_x_pub.publish(False)
+            self.pid_y_pub.publish(False)
+            null_vector = Twist()
+            self.velocity_pub.publish(null_vector)
 
 
 # Spin on control_service so that if the service goes down, we stop. This is better than doing it on
 # rospy itself because it's more specific (per
 # http://wiki.ros.org/rospy/Overview/Services#A.28Waiting_for.29_shutdown)
 if __name__ == "__main__":
-  rospy.init_node('position_control')
-  C = Controller()
-  C.control_service.spin()
+    rospy.init_node('position_control')
+    C = Controller()
+    C.control_service.spin()
