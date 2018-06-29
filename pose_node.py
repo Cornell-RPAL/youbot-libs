1b2216ce0effe83adfdab74890349968e3599463
Wil Thomason <wil.thomason@gmail.com>
2018-06-29 15:10:15 -0400

    Working style and stopping feature rewrite for position control

--- pose_node.py
+++ pose_node.py
@@ -2,32 +2,33 @@
 
 import rospy
 import tf
-from std_msgs.msg import Float64, String
+from std_msgs.msg import Float64
+
 
 class PoseNode(object):
+  '''Node responsible for republishing X and Y components of pose on independent topics for PID
+  controllers'''
 
-  def __init__(self, transform_target='world', transform_source='vicon/Mufasa_Base/Mufasa_Base'):
+  def __init__(self, transform_target='world', transform_source='base_link'):
     self.transform_target = transform_target
     self.transform_source = transform_source
     self.pub_x = rospy.Publisher('x_pid', Float64, queue_size=10)
     self.pub_y = rospy.Publisher('y_pid', Float64, queue_size=10)
-    self.pub_pos = rospy.Publisher('pos', String, queue_size=10)
     self.listen = tf.TransformListener()
 
   def broadcaster(self):
+    '''Listen for pose transform and republish coordinates once'''
     try:
-      (self._pos, rotation) = self.listen.lookupTransform(self.transform_target,
-                                                            self.transform_source, rospy.Time(0))
-      rospy.loginfo("Successfully looked up transform!")
-      self.pub_x.publish(self._pos[0])
-      self.pub_y.publish(self._pos[1])
-      self.pub_pos.publish(str(self._pos))
-      rospy.loginfo("Successfully published transform info to both PIDS!")
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+      pos, _ = self.listen.lookupTransform(self.transform_target, self.transform_source,
+                                           rospy.Time(0))
+      self.pub_x.publish(pos[0])
+      self.pub_y.publish(pos[1])
+    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as err:
       rospy.logerr("Failed to lookup transform between {} and {}! Got error: {}".format(
-          self.transform_source, self.transform_target, e))
+          self.transform_source, self.transform_target, err))
 
   def wait(self):
+    '''Wait for the pose transform to start publishing'''
     rospy.loginfo('Waiting for transform...')
     self.listen.waitForTransform(self.transform_target, self.transform_source, rospy.Time(),
                                  rospy.Duration(5))
--- position_control.py
+++ position_control.py
@@ -1,36 +1,69 @@
 #!/usr/bin/env python2.7
 
+from functools import partial
+
+import numpy as np
 import rospy
-import numpy
-import ast
+import tf
 from geometry_msgs.msg import Twist
-from std_msgs.msg import Float64, Bool, String
+from std_msgs.msg import Bool, Float64
+
 from youbot_position.srv import PositionControl, PositionControlResponse
 
 
 class Controller(object):
   '''A position control service node'''
 
-  def __init__(self):
-    # setpoint_topic='setpoint_x', control_topic='control_x'):
-    # set called to False initially, and iniialize publishers that publish
-    # intended setpoint and the desired velocity for the youBot.
-    self.set_velocity = False
-    self.fresh_x = False
-    self.fresh_y = False
-    self.setpoint_pub_x = rospy.Publisher('setpoint_x', Float64, queue_size=5, latch=True)
-    self.setpoint_pub_y = rospy.Publisher('setpoint_y', Float64, queue_size=5, latch=True)
-    self.velocity_pub = rospy.Publisher('mufasa/cmd_vel', Twist, queue_size=10)
-    # Initialize publishers to pid_enable topics
-    self.pid_x_pub = rospy.Publisher('enable_x', Bool, queue_size=10)
-    self.pid_y_pub = rospy.Publisher('enable_y', Bool, queue_size=10)
+  def __init__(self,
+               global_frame='world',
+               youbot_frame='base_link',
+               enable_topic_format='enable_{}',
+               setpoint_topic_format='setpoint_{}',
+               control_topic_format='control_{}'):
+    # Initialize constants
+    self.stopping_distance = rospy.get_param('stopping_distance', 0.05)
+
+    # Initialize system state
+    self.fresh_val = {'x': False, 'y': False}
     self.velocity = Twist()
-    # Initialize subscriber that listens to output of PIDs
-    self.control_sub_x = rospy.Subscriber('control_x', Float64, self.control_callback_x)
-    self.control_sub_y = rospy.Subscriber('control_y', Float64, self.control_callback_y)
-    # Initialize subsriber to pose topic
-    self.pose_sub = rospy.Subscriber('pos', String, self.pose_callback)
+    self.frames = {'target': global_frame, 'source': youbot_frame}
+    self.goal = np.zeros(2)
+
+    # Setup publishers for setpoints, velocity, and PID enabling
+    self.setpoint_pub = {
+        'x': rospy.Publisher(setpoint_topic_format.format('x'), Float64, queue_size=5, latch=True),
+        'y': rospy.Publisher(setpoint_topic_format.format('y'), Float64, queue_size=5, latch=True)
+    }
+
+    self.velocity_pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+    self.pid_enable_pub = {
+        'x': rospy.Publisher(enable_topic_format.format('x'), Bool, queue_size=10),
+        'y': rospy.Publisher(enable_topic_format.format('y'), Bool, queue_size=10)
+    }
+
+    # Setup subscribers for control effort
+    self.control_sub = {
+        'x':
+            rospy.Subscriber(
+                control_topic_format.format('x'), Float64, partial(self.control_callback, 'x')),
+        'y':
+            rospy.Subscriber(
+                control_topic_format.format('y'), Float64, partial(self.control_callback, 'y'))
+    }
+
+    # Start with PID disabled
+    self.disable_control()
+
+    # Setup transform listener for pose transform
+    self.tf_listener = tf.TransformListener()
+
+    # Setup timer to disable control when the setpoint is reached
+    self.prox_timer = rospy.Timer(rospy.Duration(0.2), self.pose_callback)
+
     # Initialize service that will accept requested positions and set PID setpoints to match
+    rospy.loginfo('Waiting for transform from {} to {}...'.format(global_frame, youbot_frame))
+    self.tf_listener.waitForTransform(self.frames['target'], self.frames['source'], rospy.Time(),
+                                      rospy.Duration(15))
     self.control_service = rospy.Service('position_control', PositionControl,
                                          self.position_control_service)
     rospy.loginfo("Position control service running!")
@@ -39,49 +72,46 @@ class Controller(object):
   def position_control_service(self, req):
     '''Callback receiving control service requests'''
     if req.stop:
-      null_vector = Twist()
-      self.velocity_pub.publish(null_vector)
-      self.set_velocity = False
+      self.disable_control()
+      self.velocity_pub.publish(Twist())
       return PositionControlResponse()
 
-    rospy.loginfo("Received request: %s", req)
-    self.setpoint_pub_x.publish(req.x)
-    self.setpoint_pub_y.publish(req.y)
-    self.pid_x_pub.publish(True)
-    self.pid_y_pub.publish(True)
-    self.set_velocity = True
+    rospy.logdebug("Received request: %s", req)
+    self.setpoint_pub['x'].publish(req.x)
+    self.setpoint_pub['y'].publish(req.y)
+    for controller_name in self.pid_enable_pub:
+      self.pid_enable_pub[controller_name].publish(True)
     return PositionControlResponse()
 
-  # Check if service has been called. If so, while listening to control_topic,
-  # set our linear velocity to the data and publish our current velocity.
-  def control_callback_x(self, control_x):
-    '''Callback receiving x PID control output'''
-    if self.set_velocity:
-      self.velocity.linear.x = control_x.data
-      self.fresh_x = True
-      if self.fresh_y:
-        self.velocity_pub.publish(self.velocity)
-        self.fresh_x = False
+  def disable_control(self):
+    '''Disable the PID controllers'''
+    for controller_name in self.pid_enable_pub:
+      self.pid_enable_pub[controller_name].publish(False)
 
-  def control_callback_y(self, control_y):
-    '''Callback receiving y PID control output'''
-    if self.set_velocity:
-      self.velocity.linear.y = control_y.data
-      self.fresh_y = True
-      if self.fresh_x:
-        self.velocity_pub.publish(self.velocity)
-        self.fresh_y = False
+  def control_callback(self, dimension, control):
+    '''Callback receiving PID control output'''
+    if dimension == 'x':
+      self.velocity.linear.x = control.data
+    elif dimension == 'y':
+      self.velocity.linear.y = control.data
 
-  def pose_callback(self, pos):
-    pose = ast.literal_eval(str(pos)[7:-21])
-    dist = numpy.linalg.norm(pose)
-    rospy.loginfo("Got distance: %s", dist)
-    if (dist <= 0.05):
-      self.set_velocity = False
-      self.pid_x_pub.publish(False)
-      self.pid_y_pub.publish(False)
-      null_vector = Twist()
-      self.velocity_pub.publish(null_vector)
+    self.fresh_val[dimension] = True
+    if self.fresh_val['x'] and self.fresh_val['y']:
+      self.velocity_pub.publish(self.velocity)
+      self.fresh_val['x'] = False
+      self.fresh_val['y'] = False
+
+  def pose_callback(self, _):
+    '''Handle stopping if the current pose is close enough to the goal'''
+    (pose_x, pose_y, _), _ = self.tf_listener.lookupTransform(self.frames['target'],
+                                                              self.frames['source'], rospy.Time())
+    pose = np.array([pose_x, pose_y])
+    diff = pose - self.goal
+    dist = diff.dot(diff)
+    rospy.logdebug('Got distance: %s', dist)
+    if dist <= self.stopping_distance:
+      self.disable_control()
+      self.velocity_pub.publish(Twist())
 
 
 # Spin on control_service so that if the service goes down, we stop. This is better than doing it on
